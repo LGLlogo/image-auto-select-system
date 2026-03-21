f796a3df953b0fcade1ad4e7eaae31789398a8ab
LG <liangguan.lo.go@gmail.com>
2026-03-22 01:54:09 +0800

    新增图片数据源（夸克quark）

--- quark/quark.py
+++ quark/quark.py
@@ -8,21 +8,20 @@ def download_images_from_quark(folder_name: str, desc_save_dir: str, logger):
     with QuarkClient() as client:
         # 搜索文件
         results = client.search_files(folder_name, size=1)
-        logger.info(f"找到 {len(results['data']['list'])} 个文件")
+        logger.info('download_images', f"找到 {len(results['data']['list'])} 个文件")
         # print(results['data']['list'])
         folder_id = results['data']['list'][0]['fid']
         files = client.list_files(folder_id, size=20)
         files = files['data']['list']
         # print(len(files))
         file_ids = [file['fid'] for file in files]
-        logger.info(file_ids)
+        logger.info('download_images', file_ids)
 
         def progress_callback(current, total, downloaded_bytes, total_bytes):
             bar_length = int((abs(downloaded_bytes) / total_bytes) * 10)
             bar_str = "█" * bar_length + "-" * (10 - bar_length)
-            logger.info(
-                f"image[{current}/{total}] download Progress: | {bar_str} | {downloaded_bytes / total_bytes * 100:.2f}"
-                f" % Complete")
+            logger.info('download_images', f"image[{current}/{total}] download Progress: | {bar_str} | "
+                                           f"{downloaded_bytes / total_bytes * 100:.2f} % Complete")
 
         client.download.download_files(file_ids=file_ids, save_dir=desc_save_dir, progress_callback=progress_callback)
         return len(file_ids)
