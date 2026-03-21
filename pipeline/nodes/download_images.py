4a1845746598d1665128a3795df8e7cd173193c6
LG <liangguan.lo.go@gmail.com>
2026-03-22 02:11:16 +0800

    新增图片数据源（夸克quark）

--- pipeline/nodes/download_images.py
+++ pipeline/nodes/download_images.py
@@ -12,8 +12,7 @@ class DownloadImagesNode(Node):
         self.input_image_dir = input_image_dir
 
     def run(self, ctx):
-        logger = ctx.get("logger")
-        l = download_images_from_quark(folder_name=self.folder_name, desc_save_dir=self.input_image_dir, logger=logger)
+        l = download_images_from_quark(folder_name=self.folder_name, desc_save_dir=self.input_image_dir)
         super().log(ctx, f"Download images: {l}")
         path = Path(self.input_image_dir)
         # 保存绝对路径
--- quark/quark.py
+++ quark/quark.py
@@ -4,36 +4,37 @@ from pathlib import Path
 from quark_client import QuarkClient
 
 
-def download_images_from_quark(folder_name: str, desc_save_dir: str, logger):
+def download_images_from_quark(folder_name: str, desc_save_dir: str):
     with QuarkClient() as client:
         # 搜索文件
         results = client.search_files(folder_name, size=1)
-        logger.info('download_images', f"找到 {len(results['data']['list'])} 个文件")
+        print(f"找到 {len(results['data']['list'])} 个文件")
         # print(results['data']['list'])
         folder_id = results['data']['list'][0]['fid']
         files = client.list_files(folder_id, size=20)
         files = files['data']['list']
         # print(len(files))
         file_ids = [file['fid'] for file in files]
-        logger.info('download_images', file_ids)
+        print(file_ids)
 
         def progress_callback(current, total, downloaded_bytes, total_bytes):
-            bar_length = int((abs(downloaded_bytes) / total_bytes) * 10)
-            bar_str = "█" * bar_length + "-" * (10 - bar_length)
-            logger.info('download_images', f"image[{current}/{total}] download Progress: | {bar_str} | "
-                                           f"{downloaded_bytes / total_bytes * 100:.2f} % Complete")
+            pass
+            # bar_length = int((abs(downloaded_bytes) / total_bytes) * 10)
+            # bar_str = "█" * bar_length + "-" * (10 - bar_length)
+            # logger.info('download_images', f"image[{current}/{total}] download Progress: | {bar_str} | "
+            #                                f"{downloaded_bytes / total_bytes * 100:.2f} % Complete")
 
         client.download.download_files(file_ids=file_ids, save_dir=desc_save_dir, progress_callback=progress_callback)
         return len(file_ids)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.StreamHandler()
-        ]
-    )
-    logger = logging.getLogger(__name__)
-    download_images_from_quark(folder_name='0104相机', desc_save_dir='input_images', logger=logger)
+    # logging.basicConfig(
+    #     level=logging.INFO,
+    #     format='%(asctime)s - %(levelname)s - %(message)s',
+    #     handlers=[
+    #         logging.StreamHandler()
+    #     ]
+    # )
+    # logger = logging.getLogger(__name__)
+    download_images_from_quark(folder_name='0104相机', desc_save_dir='input_images')
